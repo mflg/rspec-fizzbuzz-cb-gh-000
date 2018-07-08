9c8bc29e9241f380cd2f15f131772ce6e0f60433
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-08 00:14:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  return = ""
-  if number % 3 == 0
-    return = return + "Fizz"
+  if number%3==0 && number%5==0
+    "FizzBuzz"
+  elseif number % 3 == 0
+    "Fizz"
   elseif number % 5 == 0
     "Buzz"
   else
