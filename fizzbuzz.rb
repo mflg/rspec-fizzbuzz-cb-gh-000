06955ab13ef4f1325f8f5cb55373cf0668de169a
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-08 00:18:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if number%3==0 && number%5==0
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elsif number % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif number % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   else
-    puts nil
+    return nil
   end
 end
 
