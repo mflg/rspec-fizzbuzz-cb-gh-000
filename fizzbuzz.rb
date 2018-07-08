662176f1928c43e7be4eb1204931267d7daa3e4d
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-08 00:23:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,7 @@ def fizzbuzz(number)
   if number%3==0 && number%5==0
     "FizzBuzz"
   end
+  
   if number % 3 == 0
     "Fizz"
   end
@@ -12,5 +13,5 @@ def fizzbuzz(number)
   if number % 5 == 0
     "Buzz"
   end
-  
+
 end
